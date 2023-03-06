2ae21924027d0baff7f57c5ac60cf866adf7e85f
Ton Silva <silvatonsp@gmail.com>
2023-03-06 16:41:22 +0100

    Day-004: Type Annotations

---
+++ Day-004/MyPlayground.playground/Contents.swift
@@ -0,0 +1,37 @@
+import Cocoa
+
+// MARK: - How to use type annotations
+
+let surname: String = "Lasso"
+var score: Int = 0
+
+let playerName: String = "Roy"
+let luckyNumer: Int = 13
+let pi: Double = 3.141
+var isAuthenticated: Bool = true
+
+var albums: [String] = ["Red", "Fearless"]
+var user: [String: String] = ["id": "@twostraws"]
+var books: Set<String> =  Set([
+    "The Bluest Eye",
+    "Foundation",
+    "Girl, Woman, Other"
+])
+
+var soda: [String] = ["Coke", "Pepsi", "Irn-Bru"]
+var teams: [String] = [String]()
+var cities: [String] = []
+var clues = [String]()
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style: UIStyle = UIStyle.light
+style = .dark
+
+let username: String
+// lots of complex logic
+username = "twostraws"
+// lots more complex logic
+print(username)
